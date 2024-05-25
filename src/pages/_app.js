9c1f0e28e1f5ac2eba1ba73781c820217f18c3fe
markoangelovski <angelovski.marko@gmail.com>
2024-05-25 19:16:59 +0200

    Added page title and WP link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,25 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main
-      className={`flex max-w-screen-2xl mx-auto flex-col p-24 ${inter.className}`}
-    >
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Link href={"https://wp-preview-wp.angelovski.top/admin"} target="blank">
+        <Image src={"/w-logo-blue-white-bg.png"} width={30} height={30} />
+      </Link>
+      <Head>
+        <title>WP Preview</title>
+      </Head>
+      <main
+        className={`flex max-w-screen-2xl mx-auto flex-col p-24 ${inter.className}`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
